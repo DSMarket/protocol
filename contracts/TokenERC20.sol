e991bbdfb50333257c2cd5e065db86b22eba35b4
frani <franinnocenti@gmail.com>
2024-05-28 00:50:47 -0300

    chore: ch ERC20 name

--- contracts/TokenERC20.sol
+++ contracts/TokenERC20.sol
@@ -7,7 +7,7 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SFAT is ERC20 {
-    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
+    constructor(uint256 initialSupply) ERC20("Storage Forward Agreement Token", "SFAT") {
         _mint(msg.sender, initialSupply);
     }
 }
