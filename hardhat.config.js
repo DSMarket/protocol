f6539ea80e7b67a90327990113c881aa546cdb6e
frani <franinnocenti@gmail.com>
2024-05-30 23:32:00 -0300

    feat: add deploy and verify scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
-
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomicfoundation/hardhat-ethers");
+require("@nomicfoundation/hardhat-verify");
 require("@nomiclabs/hardhat-solhint");
+const { vars } = require("hardhat/config");
 
-const DEPLOYER_PK = [`${vars.get("DEPLOYER_PK")}`];
+const DEPLOYER_PK = `${vars.get("DEPLOYER_PK")}`;
 const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
-const BSC_RPC = process.env.BSC_RPC || "https://rpc.ankr.com/bsc";
-const POLYGON_RPC = process.env.POLYGON_RPC || "https://polygon-rpc.com/";
-const FANTOM_RPC = process.env.FANTOM_RPC || "https://rpcapi.fantom.network";
-
-const LOCALHOST_RPC = process.env.LOCALHOST_RPC || "http://127.0.0.1:8545";
 
 const config = {
   defaultNetwork: "localhost",
@@ -19,34 +15,28 @@ const config = {
       url: "http://127.0.0.1:8545",
       timeout: 300000,
       gas: 15_000_000,
+      accounts: [DEPLOYER_PK],
+    },
+    sepolia: {
+      url: "https://rpc.ankr.com/eth_sepolia",
+      timeout: 300000,
+      gas: 15_000_000,
+      accounts: [DEPLOYER_PK],
     },
     hardhat: {},
+  },
+  sourcify: {
+    // Disabled by default
+    // Doesn't need an API key
+    enabled: true,
   },
   etherscan: {
     // Your API key for Etherscan
     // Obtain one at https://etherscan.io/
     apiKey: ETHERSCAN_API_KEY,
   },
-  vyper: {
-    compilers: [{ version: "0.3.3" }],
-  },
   solidity: {
     compilers: [
-      {
-        version: "0.6.6",
-      },
-      {
-        version: "0.4.18",
-      },
-      {
-        version: "0.8.10",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
       {
         version: "0.8.24",
         settings: {
--- scripts/verifyContracts.js
+++ scripts/verifyContracts.js
@@ -8,15 +8,17 @@ const GAS = {
 
 const main = async () => {
   console.log("Running deploy script...");
+  const TOKEN_ADDRESS = "0x890521272a46306a1d4589a6c0c39f80813db0dc";
+  const MARKET_ADDRESS = "0x5418c03638711c7d5ed6fb34542485a1f6d8ff24";
   try {
-    console.log("\n\n===> Deploy SFA Token\n\n");
-    const token = await ethers.deployContract("SFAToken", [100_000_000]);
-    await token.waitForDeployment();
-    console.log("Token deployed at:", token.target);
-    const market = await ethers.deployContract("Market", [token.target]);
-    await market.waitForDeployment();
-    console.log("Market deployed at:", market.target);
-    console.log(await market.owner());
+    console.log("Verifying Contracts...");
+    // Verify contracts
+    console.log("\n\n===> Verifyin SFA Token");
+    await verifyContract(TOKEN_ADDRESS, [100_000_000]);
+    console.log("done");
+    console.log("\n\n===> Verifyin SFA Market");
+    await verifyContract(MARKET_ADDRESS, [TOKEN_ADDRESS]);
+    console.log("done");
   } catch (e) {
     console.log(e.message);
   }
